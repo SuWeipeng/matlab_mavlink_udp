bddd01d265a12c2e1eed9691e7af6d95f379d3c0
SuWeipeng <weipeng.su@outlook.com>
2020-02-15 21:45:57 +0800

    将采样绘图做成动画效果

--- pwm_rpm_test01.m
+++ pwm_rpm_test01.m
@@ -11,6 +11,10 @@ t.InputBufferSize = 89120;
 
 fopen(t)
 
+pwm = 0;
+rpm = 0;
+h = plot(pwm,rpm,'r.');
+
 for i = 1:1000
     tic
     A = dec2hex(fread(t));
@@ -20,6 +24,9 @@ for i = 1:1000
             rpm(i,1) = float(A,7,10);
             disp(['pwm: ',num2str(pwm(i,1)),', ' ...
                 'rpm: ', num2str(rpm(i,1))])
+            h.XData = pwm;
+            h.YData = rpm;
+            drawnow
         end
     end
     freq(i,1) = 1/toc;
